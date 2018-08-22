32914830775ad21ab090f6069a3e1d0202d9391a
kkishans <kishan7575t@gmail.com>
2018-08-22 13:53:34 +0530

    make main page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>My To-Do</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff4e1;                
+/*                font-family: 'Raleway';
+*/                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+               
+                padding: 0 25px;
+                font-size: 20px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+           
+            <div class="content">
+                <div class="title m-b-md" style="font-family: 'Raleway';">
+                    My To-Do
+                </div>
+
+                 @if (Route::has('login'))
+                
+                 @auth
+
+              
+
+                        <a href="{{ url('/home') }}" class="list-inline-item">Home</a>
+                  
+                    @else
+           
+                        <a href="{{ route('login') }}" class=" text-center btn btn-outline-primary">Login</a><br><br>
+                        <a href="{{ route('register') }}" class=" text-center btn btn-outline-primary"">Make Accoount</a>
+
+                    @endauth
+                            
+
+            @endif
+
+    
+            </div>
+        </div>
+    </body>
+</html>
