d71a84fc8adbea06cb2185dd26b4fbddea7acc44
kkishans <kishan7575t@gmail.com>
2018-08-23 22:07:19 +0530

    I make some main part

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -23,19 +23,24 @@
                 </div>
             
         </div>
-        <div class="col-sm-8">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-12">
-                        hiiiiiiiiiiiiiiiiii
-                        hi
-                    </div>   
-                    <div class="col-sm-12">
-                        hiiiiii
-                        hi
-                    </div>    
+        <div class="col-sm-8" style="padding: .5rem;">
+            <div class="container-fluid">
+            <div class="list-group">
+
+  <a href="kishan.html" class="list-group-item list-group-item-action">
+  <label class="custom-control custom-checkbox">
+      <input type="checkbox" class="custom-control-input">
+      <span class="custom-control-indicator"></span>
+      <span class="custom-control-description">this is my todo list</span>
+</label>
+</a>
+
+<input type="text-box" name="text-box" class="form-control" autofocus placeholder="+ Add To-Do" style="margin-top: .1rem;">
+  <!-- <a href="#" class="list-group-item list-group-item-action">Show TV shows</a>
+  <a href="#" class="list-group-item list-group-item-action">Take dinner</a>
+  <a href="#" class="list-group-item list-group-item-action">Do work on to-do project</a> -->
+        </div>
                     
-                </div>
             </div>
         </div>
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,7 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/design.css') }}">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 </head>
 <body>
     <div id="app">
