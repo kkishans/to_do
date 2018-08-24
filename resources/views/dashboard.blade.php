3f8d10d6448a3e034e56c6e5a5f490ce9f5a97c8
nnikunjt <38680557+nnikunjt@users.noreply.github.com>
2018-08-24 22:09:59 +0530

    set img nav bar and try to set margin
    working on it

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    <title>Collapsible sidebar using Bootstrap 4</title>
+    <title>My To-Do</title>
 
     <!-- Bootstrap CSS CDN -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
@@ -36,8 +36,14 @@
                     <span>My Day</span>
                 </li>
                 <li>
-                    <i class="far fa-check-circle"></i>
+                    <i class="fas fa-check"></i>
                     <span>TO-DO</span>
+                </li>
+            </ul>
+            <ul class="list-unstyled components">
+                <li>
+                    <i class="fas fa-plus"></i>
+                    <span>New-List</span>
                 </li>
             </ul>
         </nav>
@@ -62,8 +68,13 @@
                     </div>
                 </div>
             </nav>
-
-            
+           
+            <div class="container-fluid" style="margin-left: -20px;margin-top: -35px">
+                <div class="row">
+                    <img src="{{ asset('img/back.gif') }}" width="110%" height="200px">                         
+                </div>
+            </div> 
+        </div>        
 
     <!-- jQuery CDN - Slim version (=without AJAX) -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
