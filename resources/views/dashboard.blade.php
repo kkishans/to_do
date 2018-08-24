279bc66ceb5e495e2e19ee993f726e8fa5f7c909
nnikunjt <38680557+nnikunjt@users.noreply.github.com>
2018-08-24 21:24:36 +0530

    built dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,50 +1,84 @@
-@extends('layouts.app')
 
-@section('content')
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-sm-4">
-           
-                <div class="wrapper">
-                    <nav id="sidebar">
-                        <ul class="list-unstyled components">
-                            <li class="active">
-                                <a href="#" class="nav-link text-dark">My Day</a>
-                            </li>
-                            <li>
-                                <a href="#" class="nav-link text-dark"><img src="{{ asset('img/to_do.png') }}" width="12%">To-Do</a>
-                            </li>
-                            <hr>
-                            <li>
-                                <a href="#" class="nav-link text-dark"><img src="{{ asset('img/add-list.png') }}" width="12%">New-List</a>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            
-        </div>
-        <div class="col-sm-8" style="padding: .5rem;">
-            <div class="container-fluid">
-            <div class="list-group">
+<!DOCTYPE html>
+<html>
 
-  <a href="kishan.html" class="list-group-item list-group-item-action">
-  <label class="custom-control custom-checkbox">
-      <input type="checkbox" class="custom-control-input">
-      <span class="custom-control-indicator"></span>
-      <span class="custom-control-description">this is my todo list</span>
-</label>
-</a>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-<input type="text-box" name="text-box" class="form-control" autofocus placeholder="+ Add To-Do" style="margin-top: .1rem;">
-  <!-- <a href="#" class="list-group-item list-group-item-action">Show TV shows</a>
-  <a href="#" class="list-group-item list-group-item-action">Take dinner</a>
-  <a href="#" class="list-group-item list-group-item-action">Do work on to-do project</a> -->
-        </div>
-                    
+    <title>Collapsible sidebar using Bootstrap 4</title>
+
+    <!-- Bootstrap CSS CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <!-- Our Custom CSS -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/design.css') }}">
+
+    <!-- Font Awesome JS -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+
+    <div class="wrapper">
+        <!-- Sidebar  -->
+        <nav id="sidebar">
+            <div class="sidebar-header">
+                <h3>My To-DO</h3>
+                <strong>Do</strong>
             </div>
-        </div>
-    </div>
-</div>
 
+            <ul class="list-unstyled components">
+                <li>
+                    <i class="fas fa-sun"></i>
+                    <span>My Day</span>
+                </li>
+                <li>
+                    <i class="far fa-check-circle"></i>
+                    <span>TO-DO</span>
+                </li>
+            </ul>
+        </nav>
 
-@endsection
+        <!-- Page Content  -->
+        <div id="content">
+
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+
+                    <button type="button" id="sidebarCollapse" class="btn btn-info">
+                        <i class="fas fa-bars"></i>
+                    </button>
+                    <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <i class="fas fa-align-justify"></i>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="nav navbar-nav ml-auto">
+                            
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+            
+
+    <!-- jQuery CDN - Slim version (=without AJAX) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- Popper.JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#sidebarCollapse').on('click', function () {
+                $('#sidebar').toggleClass('active');
+            });
+        });
+    </script>
+</body>
+
+</html>
