50c3a04f08a0adfebe4ce1089fd16dd9c04f9d83
kkishans <kishan7575t@gmail.com>
2018-08-28 23:05:45 +0530

    i set some todo examples in main

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -2,5 +2,49 @@
 
 @section('content')
 
-<h1>now we can add here our some text...</h1>
+	<div class="container-fluid text-left">
+		<ul class="list-group list-group-flush">
+		  	<li class="list-group-item">
+		 		<a href="kishan.html">
+		 			<label class="container">this is my first to do
+  					<input type="checkbox">
+  					<span class="checkmark"></span>
+				</label>
+		 		</a>
+		  		
+		  	</li>
+		  	<li class="list-group-item">
+		 		<a href="kishan.html">
+		 			<label class="container">this is my second to do
+  					<input type="checkbox">
+  					<span class="checkmark"></span>
+				</label>
+		 		</a>
+		  		
+		  	</li>
+		  	<li class="list-group-item">
+		 		<a href="kishan.html">
+		 			<label class="container">this is my third to do
+  					<input type="checkbox">
+  					<span class="checkmark"></span>
+				</label>
+		 		</a>
+		  		
+		  	</li>
+		  	<li class="list-group-item">
+		 		<a href="kishan.html">
+		 			<label class="container">this is my last to do
+  					<input type="checkbox">
+  					<span class="checkmark"></span>
+				</label>
+		 		</a>
+		  		
+		  	</li>
+  	<input type="text" name="todobox" placeholder=" + Add To-DO" style="border:0; width:80%; font-size: 20px; margin-left: 3rem; margin-top: 1rem; ">
+  </li>
+</ul>
+	</div>
+
+
+
 @endsection
