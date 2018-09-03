18aa22b1eda9bb7866059d46846e7eb1ded835a4
kkishans <kishan7575t@gmail.com>
2018-09-03 13:36:14 +0530

    Add soome basic for add todo

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,45 +4,26 @@
 
 	<div class="container-fluid text-left">
 		<ul class="list-group list-group-flush">
-		  	<li class="list-group-item">
-		 		<a href="kishan.html">
-		 			<label class="container">this is my first to do
-  					<input type="checkbox">
-  					<span class="checkmark"></span>
-				</label>
-		 		</a>
-		  		
-		  	</li>
-		  	<li class="list-group-item">
-		 		<a href="kishan.html">
-		 			<label class="container">this is my second to do
-  					<input type="checkbox">
-  					<span class="checkmark"></span>
-				</label>
-		 		</a>
-		  		
-		  	</li>
-		  	<li class="list-group-item">
-		 		<a href="kishan.html">
-		 			<label class="container">this is my third to do
-  					<input type="checkbox">
-  					<span class="checkmark"></span>
-				</label>
-		 		</a>
-		  		
-		  	</li>
-		  	<li class="list-group-item">
-		 		<a href="kishan.html">
-		 			<label class="container">this is my last to do
-  					<input type="checkbox">
-  					<span class="checkmark"></span>
-				</label>
-		 		</a>
-		  		
-		  	</li>
-  	<input type="text" name="todobox" placeholder=" + Add To-DO" style="border:0; width:80%; font-size: 20px; margin-left: 3rem; margin-top: 1rem; ">
-  </li>
-</ul>
+		  	<form id="form" >
+					<div class="input-group">
+						<input type="text" id ="input" name="todobox" placeholder=" + Add To-DO" style="border:0; width:80%; font-size: 20px; margin-left: 3rem; margin-top: 1rem; ">
+						<span>
+							<button id="btn" type="button" class="btn btn-primary" style=" margin-left: 3rem; margin-top: 1rem; ">Submit</button>
+						</span>
+				</div>			     
+			</form>
+		</ul>
+	</div>
+	<div class="row">				
+				<ul id="list" type="none" class="list-group list-group-flush">
+					<li ><input type="checkbox" class="checkbox" hidden></li>
+				
+				</ul>
+			</div>
+			<div class="row">
+				<button id="btnClr" type="button" class="btn btn-primary mx-auto btnHide">Clear</button>
+			</div>				
+		</div>
 	</div>
 
 
